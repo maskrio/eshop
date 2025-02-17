37eb43a8f665177948a805f89a4c334c249eb2be
maskrio <yosemaranata@gmail.com>
2025-02-17 16:24:09 +0700

    feat: add ApplicationTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertNotNull(context, "The application context should have loaded correctly.");
     }
 
 }
