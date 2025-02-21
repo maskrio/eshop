f022e669ff09c242db896d721cf346abf5b043f9
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-02-21 23:17:08 +0700

    fix: coverage 100%

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTest.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EshopApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        EshopApplication.main(new String[] {});
+    }
+}
