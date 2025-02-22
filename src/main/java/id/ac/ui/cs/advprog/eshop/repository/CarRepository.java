7978dede991b90c629662340d512af9fc75ac7f8
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-02-22 15:53:04 +0700

    fix: using basic functionality instead of changing every single attribute, better for maintainability

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,5 +1,6 @@
 
 package id.ac.ui.cs.advprog.eshop.repository;
+
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
 
@@ -10,39 +11,42 @@ import java.util.UUID;
 
 @Repository
 public class CarRepository {
-  static int id = 0;
-  private List<Car> carData = new ArrayList<>();
-  public Car create(Car car) {
-    if (car.getCarId() == null) {
-      UUID uuid = UUID.randomUUID();
-      car.setCarId(uuid.toString());
-    }
-    carData.add(car);
-    return car;
-  }
-  public Iterator<Car> findAll() {
-    return carData.iterator();
-  }
-  public Car findById(String id) {
-    for (Car car : carData) {
-      if (car.getCarId().equals(id)) {
-        return car;
-      }
-    }
-    return null;
-  }
-  public Car update(String id, Car updateCar) {
-    for (Car car : carData) {
-      if (car.getCarId().equals(id)) {
-        car.setCarName(updateCar.getCarName());
-        car.setCarColor(updateCar.getCarColor());
-        car.setCarQuantity(updateCar.getCarQuantity());
-        return car;
-      }
-    }
-    return null;
-  }
-  public void delete(String id) {
-    carData.removeIf(car -> car.getCarId().equals(id));
-  }
+	static int id = 0;
+	private List<Car> carData = new ArrayList<>();
+
+	public Car create(Car car) {
+		if (car.getCarId() == null) {
+			UUID uuid = UUID.randomUUID();
+			car.setCarId(uuid.toString());
+		}
+		carData.add(car);
+		return car;
+	}
+
+	public Iterator<Car> findAll() {
+		return carData.iterator();
+	}
+
+	public Car findById(String id) {
+		for (Car car : carData) {
+			if (car.getCarId().equals(id)) {
+				return car;
+			}
+		}
+		return null;
+	}
+
+	public Car update(String id, Car updateCar) {
+		for (Car car : carData) {
+			if (car.getCarId().equals(id)) {
+				car.update(updateCar);
+				return car;
+			}
+		}
+		return null;
+	}
+
+	public void delete(String id) {
+		carData.removeIf(car -> car.getCarId().equals(id));
+	}
 }
