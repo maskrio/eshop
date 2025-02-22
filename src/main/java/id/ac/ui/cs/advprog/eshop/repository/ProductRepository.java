5b80d9858f318f3743fd78085968836ce673af30
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-02-22 16:23:32 +0700

    feat: Product Repository as interface, add implementation of it

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,47 +3,14 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
+
 
 @Repository
-public class ProductRepository {
-    private List<Product> productData = new ArrayList<>();
-    public Product create(Product product){
-        productData.add(product);
-        return product;
-    }
-
-    public Iterator<Product> findAll() {
-        return productData.iterator();
-    }
-
-    public Product findById(String productId) {
-        for (Product product : productData) {
-            if (product.getProductId().equals(productId)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public Product edit(Product product) {
-        for (int i = 0; i < productData.size(); i++) {
-            if (productData.get(i).getProductId().equals(product.getProductId())) {
-                productData.set(i, product);
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public Product delete(String productId) {
-        Product product = productData.stream()
-                .filter(p -> p.getProductId().equals(productId))
-                .findFirst()
-                .orElse(null);
-        productData.remove(product);
-        return product;
-    }
+public interface ProductRepository {
+    public Product create(Product product);
+    public Iterator<Product> findAll();
+    public Product findById(String productId);
+    public Product edit(Product product);
+    public Product delete(String productId);
 }
