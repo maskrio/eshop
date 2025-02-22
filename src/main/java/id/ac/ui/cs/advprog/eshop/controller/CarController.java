c19231e08da9333fd89af732575e16c57a68deaf
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-02-22 16:01:03 +0700

    fix: CarController should depend on Interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
 import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/car")
 public class CarController {
+    private final CarService carService;
+
     @Autowired
-    private CarServiceImpl carService;
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/createCar")
     public String createCarPage(Model model) {
