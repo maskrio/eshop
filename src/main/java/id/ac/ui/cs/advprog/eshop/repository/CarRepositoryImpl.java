e4cd7cf0e52fae3ac20a277bf96fc8f9118a35f2
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-02-23 21:39:37 +0700

    fix: remove unused imports

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryImpl.java
@@ -2,7 +2,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 
 import org.springframework.stereotype.Repository;
 
