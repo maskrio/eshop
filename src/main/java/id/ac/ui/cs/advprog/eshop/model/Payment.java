3e123841eb96dc943545fb982f6c9f1550386199
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-03-07 14:44:26 +0700

    feat: [GREEN] implement voucher and bank transfer validations

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -23,7 +23,7 @@ public class Payment {
         this.setMethod(method);
         this.order = order;
         this.setStatus(PaymentStatus.PENDING.getValue());
-        this.setPaymentData(paymentData);
+        this.setPaymentData(method, paymentData);
     }
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
@@ -47,11 +47,81 @@ public class Payment {
         }
     }
 
-    public void setPaymentData(Map<String, String> paymentData) {
-        if(paymentData.isEmpty()) {
-            throw new IllegalArgumentException("Empty Payment Data");
-        }else {
+    public void setPaymentData(String method, Map<String, String> paymentData) {
+        if(validatePaymentData(method, paymentData)){
+            this.status = PaymentStatus.SUCCESS.getValue();
             this.paymentData = paymentData;
         }
+        else {
+            this.setStatus(PaymentStatus.REJECTED.getValue());
+        }
+        
     }
+
+    private boolean validatePaymentData(String method, Map<String, String> paymentData) {
+        if(paymentData.isEmpty()) {
+            return false;
+        }
+        
+        if(method.equals(PaymentMethod.VOUCHER.getValue())) {
+            if(! validateVoucherPayment(paymentData)) {
+                return false;
+            }
+        }
+
+        if(method.equals(PaymentMethod.BANK_TRANSFER.getValue())) {
+            if(! validateBankTransferPayment(paymentData)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateVoucherCode(String voucherCode) {
+        if (voucherCode.length() != 16) {
+            return false;
+        }
+
+        if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+
+        int numericCount = 0;
+        for(char ch : voucherCode.toCharArray()) {
+            if(Character.isDigit(ch)) numericCount++;
+        }
+
+        if(numericCount != 8) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateVoucherPayment(Map<String,String> paymentData) {
+        if (paymentData.get("voucherCode") == null) {
+            return false;
+        }
+
+        String voucherCode = paymentData.get("voucherCode");
+
+        if(!validateVoucherCode(voucherCode)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateBankTransferPayment(Map<String,String> paymentData) {
+        if (paymentData.get("bankName") == null || paymentData.get("bankName").isBlank()) {
+            return false;
+        }
+
+        if (paymentData.get("referenceCode") == null || paymentData.get("referenceCode").isBlank()) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
