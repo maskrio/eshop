8016b0a72536eca6fcc58a8b8166c4ff08dd611c
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-03-07 16:51:23 +0700

    fix: [GREEN] implement payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -2,21 +2,33 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import java.util.List;
+import java.util.UUID;
 
 public class PaymentServiceImpl {
+    @Autowired
+    private PaymentRepository PaymentRepository;
+
     public Payment addPayment(Order order, String method, Map<String, String> paymentData){
-        return null;
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, order, paymentData);
+        return PaymentRepository.save(payment);
     }
+
     public Payment setStatus(Payment payment, String status){
-        return null;
+        payment.setStatus(status);
+        return PaymentRepository.save(payment);
     }
+
     public Payment getPayment(String paymentId){
-        return null;
+        return PaymentRepository.findById(paymentId);
     }
+
     public List<Payment> getAllPayments(){
-        return null;
+        return PaymentRepository.getAllPayments();
     }
 }
