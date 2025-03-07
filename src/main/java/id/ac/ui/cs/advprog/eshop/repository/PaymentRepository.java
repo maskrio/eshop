465c2cb6359457cbbd3a034288bd658748624449
Yose Yehezkiel Maranata <yosemaranata@gmail.com>
2025-03-07 19:17:27 +0700

    [REFACTOR] improve Payment Repository Implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,30 +10,31 @@ import java.util.ArrayList;
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
     public Payment save(Payment payment) {
-        int i = 0;
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getId().equals(payment.getId())) {
-                paymentData.remove(i);
-                paymentData.add(i, payment);
-                return payment;
-            }
-            i += 1;
+        int index = findIndexById(payment.getId());
+        if(index != -1) {
+            paymentData.remove(index);
         }
-
         paymentData.add(payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getId().equals(id)) {
-                return savedPayment;
-            }
-        }
-        return null;
+        int index = findIndexById(id);
+        if(index == -1) return null;
+        return paymentData.get(index);
     }
 
     public List<Payment> getAllPayments() { 
         return paymentData;
+    }
+
+    private int findIndexById(String id) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
